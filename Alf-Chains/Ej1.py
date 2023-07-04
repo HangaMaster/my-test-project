79dd1e290c018df3d06e207063e0ef2cea3650d7
{HangaMaster} <{adrianegeajimenez@gmail.com}>
2023-07-04 16:18:57 +0200

    Ejercicios de prueba

---
+++ Alf-Chains/Ej1.py
@@ -0,0 +1,5 @@
+name = input("Introduzca su nombre: ")
+num = int(input("Introduzca las veces que quiere repetir su nombre: "))
+print((name + "\n") * num)
+#for i in range(num):
+#    print(name)
---
+++ Alf-Chains/Ej4.py
@@ -0,0 +1,2 @@
+tel = input("Introduce un número de teléfono siguiendo el formato '+xx-xxxxxxxxx-xx': ")
+print("El número de teléfono es:", tel[4:-3])
